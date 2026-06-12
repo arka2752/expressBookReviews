aa22d097bd07b65bc1d60f66ec78b12fa7f28a18
arka2752 <arka25253@gmail.com>
2026-06-12 18:01:31 +0600

    Updated review and login response format

--- router/auth_users.js
+++ router/auth_users.js
@@ -5,22 +5,22 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{
-  let userswithsamename = users.filter((user)=>{
+const isValid = (username) => {
+  let userswithsamename = users.filter((user) => {
     return user.username === username
   });
-  if(userswithsamename.length > 0){
+  if (userswithsamename.length > 0) {
     return true;
   } else {
     return false;
   }
 }
 
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
     return (user.username === username && user.password === password)
   });
-  if(validusers.length > 0){
+  if (validusers.length > 0) {
     return true;
   } else {
     return false;
@@ -28,25 +28,25 @@ const authenticatedUser = (username,password)=>{
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+    return res.status(404).json({ message: "Error logging in" });
   }
 
-  if (authenticatedUser(username,password)) {
+  if (authenticatedUser(username, password)) {
     let accessToken = jwt.sign({
       data: password
     }, 'access', { expiresIn: 60 * 60 });
 
     req.session.authorization = {
-      accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
+      accessToken, username
+    }
+    return res.status(200).json({ message: "User successfully logged in", token: accessToken });
   } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
   }
 });
 
@@ -55,31 +55,31 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   const isbn = req.params.isbn;
   let book = books[isbn];
   if (book) {
-      let review = req.body.review;
-      let reviewer = req.session.authorization['username'];
-      if(review) {
-          book["reviews"][reviewer] = review;
-          books[isbn] = book;
-      }
-      res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+    let review = req.query.review || req.body.review;
+    let reviewer = req.session.authorization['username'];
+    if (review) {
+      book["reviews"][reviewer] = review;
+      books[isbn] = book;
+    }
+    res.json({ message: "Review added/updated successfully", reviews: book["reviews"] });
   }
-  else{
-      res.send("Unable to find book!");
+  else {
+    res.send("Unable to find book!");
   }
 });
 
 // Delete a book review
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-    const isbn = req.params.isbn;
-    let reviewer = req.session.authorization['username'];
-    let filtered_review = books[isbn]["reviews"];
-    if (filtered_review[reviewer]){
-        delete filtered_review[reviewer];
-        res.send(`Reviews for the ISBN  ${isbn} posted by the user ${reviewer} deleted.`);
-    }
-    else{
-        res.send("Can't delete, as this review has been posted by a different user or does not exist.");
-    }
+  const isbn = req.params.isbn;
+  let reviewer = req.session.authorization['username'];
+  let filtered_review = books[isbn]["reviews"];
+  if (filtered_review[reviewer]) {
+    delete filtered_review[reviewer];
+    res.send(`Reviews for the ISBN  ${isbn} posted by the user ${reviewer} deleted.`);
+  }
+  else {
+    res.send("Can't delete, as this review has been posted by a different user or does not exist.");
+  }
 });
 
 module.exports.authenticated = regd_users;
